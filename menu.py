3c3f95416403b745ff317c81e042cadace0539d6
mehrimahamiri <amirimehrimah@gmail.com>
2021-09-20 21:45:23 +0430

    first project V1

---
+++ menu.py
@@ -0,0 +1,46 @@
+#import csv
+import sys
+
+def main():
+    menu()
+def menu():
+    print("ـــــــــــــ Welcome to SELECT_UNIT  Demo ـــــــــــــــ")
+    print()
+
+    choice = input("""
+    1: Login
+    2: Logout
+
+    Please enter your choice: """)
+
+    if choice == "1" :
+        login()
+    elif choice=="2":
+        sys.exit
+    else:
+        print("You must only select NUMBER")
+        print("Please try again !")
+        menu()
+def login():
+    choice = input("""
+    1: Student
+    2: Receptionist 
+    3: EXIT
+    Please enter your choice: """)
+    
+    if choice == "1" :
+        student()
+    elif choice=="2":
+        Receptionist()
+    elif choice == "3":
+        sys.exit
+    else:
+        print("You must only select NUMBER")
+        print("Please try again !")
+        login()
+def student():
+    pass
+def Receptionist():
+    pass
+#the program is initiated, so to speak, here
+main()
